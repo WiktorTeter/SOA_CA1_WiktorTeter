5498d8a0c3ca32b958ef5b9b0e715c6320b5148c
Wiktor Teter <D00256337@student.dkit.ie>
2025-10-10 03:49:38 +0100

    Added initial service for public holiday lookup

---
+++ SOA_CA1/Services/Holidays/IHolidayService.cs
@@ -0,0 +1,6 @@
+﻿namespace SOA_CA1.Services.Holidays;
+
+public interface IHolidayService
+{
+    Task<bool> IsPublicHolidayAsync(DateOnly date, string countryCode);
+}
---
+++ SOA_CA1/Services/Holidays/NagerDateHolidayService.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http.Json;
+
+namespace SOA_CA1.Services.Holidays;
+
+public class NagerDateHolidayService : IHolidayService
+{
+    private readonly HttpClient _http;
+
+    public NagerDateHolidayService(HttpClient http)
+    {
+        _http = http;
+    }
+
+    public async Task<bool> IsPublicHolidayAsync(DateOnly date, string countryCode)
+    {
+        var year = date.Year;
+        var holidays = await _http.GetFromJsonAsync<List<NagerHoliday>>(
+            $"api/v3/PublicHolidays/{year}/{countryCode}");
+        return holidays?.Any(h => DateOnly.FromDateTime(h.Date) == date) == true;
+    }
+
+    private sealed class NagerHoliday
+    {
+        public DateTime Date { get; set; }
+        public string LocalName { get; set; } = "";
+        public string Name { get; set; } = "";
+    }
+}
+
