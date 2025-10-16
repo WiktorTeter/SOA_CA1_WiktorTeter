aaa2b0fc5942d409de04f09816d33f1bdb252713
Wiktor Teter <D00256337@student.dkit.ie>
2025-10-16 17:16:58 +0100

    added suggestions list that take currency and date inputs and suggest changes based on budget, weekday/weekend/public holiday

---
+++ SOA_CA1/Models/Suggestion.cs
@@ -0,0 +1,18 @@
+﻿namespace SOA_CA1.Models;
+
+public enum DayType
+{
+    PublicHoliday = 0,
+    Weekend = 1,
+    Weekday = 2
+}
+
+public class Suggestion : IComparable<Suggestion>
+{
+    public string Message { get; set; } = "";
+    public DayType Type { get; set; }
+    public int Priority { get; set; }
+
+    public int CompareTo(Suggestion? other)
+        => other is null ? -1 : Priority.CompareTo(other.Priority);
+}
